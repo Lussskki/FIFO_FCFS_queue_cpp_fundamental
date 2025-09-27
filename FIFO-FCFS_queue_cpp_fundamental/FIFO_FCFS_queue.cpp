70c368db129609b036a0bd4f6d5d3f211de15e38
Luka <lussskki@gmail.com>
2025-09-27 17:40:04 +0400

    queue algorithm and functions

--- FIFO-FCFS_queue_cpp_fundamental/FIFO_FCFS_queue.cpp
+++ FIFO-FCFS_queue_cpp_fundamental/FIFO_FCFS_queue.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <queue>
+using namespace std;
+
+void print_queue(queue<int> queue)
+{
+	while (!queue.empty())
+	{
+		cout << queue.front() << " ";
+		queue.pop();
+	}
+	cout << endl;
+}
 
 int main()
-{
-	
+ {
+	queue<int>my_queue;
+	my_queue.push(1);
+	my_queue.push(2);
+	my_queue.push(3);
+
+	cout << "Size is: " << my_queue.size() << std::endl;
+	cout << "First element is: " << my_queue.front() << std::endl;
+	cout << "Last element is: " << my_queue.back() << std::endl;
+
+	cout << "My queue: ";
+	print_queue(my_queue);
 }
